9f65d1e56d95980bc34c071f808bd6176786a0ac
Peter Nørlund <peter@developers.dk>
2024-09-06 01:03:03 +0200

    Reduce size of increments

--- src/music.v
+++ src/music.v
@@ -1,7 +1,5 @@
 `default_nettype none
 
-/* verilator lint_off UNUSEDSIGNAL */
-/* verilator lint_off UNUSEDPARAM */
 /* verilator lint_off WIDTHTRUNC */
 module music
     (
@@ -11,17 +9,17 @@ module music
         output wire pwm
     );
 
-    localparam INPUT_FREQUENCY = 25000000;
-    localparam SAMPLE_RATE = 200000;
-    localparam SAMPLE_SIZE = 128;
-    localparam SAMPLE_BITS = $clog2(SAMPLE_SIZE);                               // 7
-    localparam EXTRA_SAMPLE_BITS = 8;
-    localparam EXTENDED_SAMPLE_BITS = SAMPLE_BITS + EXTRA_SAMPLE_BITS;          // 12
-    localparam EXTENDED_SAMPLE_RANGE = SAMPLE_SIZE * (2 ** EXTRA_SAMPLE_BITS);  // 500
-    localparam TICK_WIDTH = 28 * SAMPLE_RATE / 256 / 4;
-    localparam SHORT_SAMPLES = TICK_WIDTH * 3;
-    localparam LONG_SAMPLES = TICK_WIDTH * 7;
-    localparam SPACE_SAMPLES = TICK_WIDTH;
+    localparam INPUT_FREQUENCY = 25000000;                                      // 25MHz input clock
+    localparam SAMPLE_RATE = 200000;                                            // 200kHz PWM rate
+    localparam SAMPLE_BITS = $clog2(INPUT_FREQUENCY / SAMPLE_RATE);             // Number of bits needed for ideal sample size ($clog2(125) = 7 bits)
+    localparam SAMPLE_SIZE = 2**SAMPLE_BITS;                                    // Binary sample size (128)
+    localparam EXTRA_SAMPLE_BITS = 8;                                           // Additional precision in bits
+    localparam EXTENDED_SAMPLE_BITS = SAMPLE_BITS + EXTRA_SAMPLE_BITS;          // Total number of sample bits (15 bits)
+    localparam EXTENDED_SAMPLE_RANGE = SAMPLE_SIZE * (2 ** EXTRA_SAMPLE_BITS);  // Sample range which is also our internal resolution (32768)
+    localparam TICK_WIDTH = 28 * SAMPLE_RATE / 256 / 4;                         // The full Nyan Cat loop is approx. 28 seconds and contains 256 beats, which we divide in 4
+    localparam SHORT_SAMPLES = TICK_WIDTH * 3;                                  // Short note gets one beat minus a space
+    localparam LONG_SAMPLES = TICK_WIDTH * 7;                                   // Long note gets two beats minus a space
+    localparam SPACE_SAMPLES = TICK_WIDTH;                                      // Space between notes
 
     localparam G_SHARP = 3'd0;
     localparam F_SHARP = 3'd1;
@@ -33,7 +31,7 @@ module music
     localparam SHORT_NOTE = 1'b0;
     localparam LONG_NOTE = 1'b1;
 
-    reg [EXTENDED_SAMPLE_BITS - 1 : 0] increments [5:0];
+    reg [6:0] increments [5:0];
     reg [3:0] melody [24:0];
     reg [EXTENDED_SAMPLE_BITS - 1 : 0] extended_sample;
     integer melody_pos;
@@ -51,12 +49,12 @@ module music
     assign pwm = (pwm_pos <= sample ? 1'b1 : 1'b0);
 
     initial begin
-        increments[G_SHARP] = EXTENDED_SAMPLE_RANGE * 415 / SAMPLE_RATE;    // 132
-        increments[F_SHARP] = EXTENDED_SAMPLE_RANGE * 370 / SAMPLE_RATE;    // 118
-        increments[D_SHARP] = EXTENDED_SAMPLE_RANGE * 311 / SAMPLE_RATE;    // 99
-        increments[D]       = EXTENDED_SAMPLE_RANGE * 294 / SAMPLE_RATE;    // 94
-        increments[C_SHARP] = EXTENDED_SAMPLE_RANGE * 277 / SAMPLE_RATE;    // 88
-        increments[B]       = EXTENDED_SAMPLE_RANGE * 247 / SAMPLE_RATE;    // 79
+        increments[G_SHARP] = EXTENDED_SAMPLE_RANGE * 415 / SAMPLE_RATE;    // 67
+        increments[F_SHARP] = EXTENDED_SAMPLE_RANGE * 370 / SAMPLE_RATE;    // 60
+        increments[D_SHARP] = EXTENDED_SAMPLE_RANGE * 311 / SAMPLE_RATE;    // 50
+        increments[D]       = EXTENDED_SAMPLE_RANGE * 294 / SAMPLE_RATE;    // 48
+        increments[C_SHARP] = EXTENDED_SAMPLE_RANGE * 277 / SAMPLE_RATE;    // 45
+        increments[B]       = EXTENDED_SAMPLE_RANGE * 247 / SAMPLE_RATE;    // 40
 
         melody[0] = {F_SHARP, LONG_NOTE};
         melody[1] = {G_SHARP, LONG_NOTE};
@@ -117,7 +115,7 @@ module music
                 end
 
                 if (do_note) begin
-                    extended_sample <= extended_sample + increments[note];
+                    extended_sample <= extended_sample + {8'b00000000, increments[note]};
                 end
                 
             end else begin
